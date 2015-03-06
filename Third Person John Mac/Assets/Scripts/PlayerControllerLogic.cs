224f0b92a4aa66ff43da81f3dc92fcf37e7378f1
Deltawolf <zachcshore@gmail.com>
2015-03-06 12:32:45 -0600

    Computer Just Crashed, Let's save!
    Woops!

---
+++ Third Person John Mac/Assets/Scripts/PlayerControllerLogic.cs
@@ -0,0 +1,115 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerControllerLogic : MonoBehaviour {
+	
+	#region Variables (private)
+
+	[SerializeField]
+	private Animator animator;
+	[SerializeField]
+	private float directionDampTime = .25f;
+	[SerializeField]
+	private ThirdPersonCameraController gamecam;
+	[SerializeField]
+	private float directionSpeed;
+	[SerializeField]
+	private float moveSpeed;
+	[SerializeField]
+	private float rotationDegreePerSecond;
+
+	//Private global only
+	private float speed = 0.0f;
+	private float direction = 0f;
+	private float horizontal = 0.0f;
+	private float vertical = 0.0f;
+	private AnimatorStateInfo stateInfo;
+
+	//Hashes
+	private int m_LocomotionId = 0;
+	
+	#endregion
+	
+	#region Properties (public)
+
+	#endregion
+	
+	#region Unity events
+	
+	
+	void Start ()
+	{
+		animator = GetComponent<Animator> ();
+		if (animator.layerCount >= 2) 
+		{
+			animator.SetLayerWeight (1, 1);
+		}
+		m_LocomotionId = Animator.StringToHash ("Base Layer.Locomotion");
+	}
+	
+	
+	void Update ()
+	{
+		if (animator) {
+			stateInfo = animator.GetCurrentAnimatorStateInfo(0);
+			horizontal = Input.GetAxis ("Horizontal");
+			vertical = Input.GetAxis ("Vertical");
+
+			StickToWorldspace (this.transform, gamecam.transform, ref direction, ref speed);
+
+			animator.SetFloat ("Speed", speed);
+			animator.SetFloat ("Direction", direction, directionDampTime, Time.deltaTime);
+		}
+	}
+
+	void FixedUpdate ()
+	{
+		if (IsInLocomotion() && ((direction >= 0 && horizontal >= 0) || (direction < 0 && horizontal < 0)))
+		{
+			Vector3 rotationAmount = Vector3.Lerp(Vector3.zero, new Vector3(0f, rotationDegreePerSecond * (horizontal < 0f ? -1f:1f), 0f), Mathf.Abs (horizontal));
+			Quaternion deltaRotation = Quaternion.Euler (rotationAmount * Time.deltaTime);
+			this.transform.rotation = (this.transform.rotation * deltaRotation);
+		}
+	}
+	
+	
+	#endregion Unity events
+	
+	#region Methods
+
+	public void StickToWorldspace(Transform root, Transform camera, ref float directionOut, ref float speedOut)
+	{
+		Vector3 rootDirection = root.forward;
+		
+		Vector3 stickDirection = new Vector3 (horizontal, 0, vertical);
+		
+		speedOut = stickDirection.sqrMagnitude * moveSpeed;
+		
+		Vector3 CameraDirection = camera.forward;
+		CameraDirection.y = 0.0f;
+		Quaternion referentialShift = Quaternion.FromToRotation(Vector3.forward, CameraDirection);
+		
+		Vector3 moveDirection = referentialShift * stickDirection;
+		Vector3 axisSign = Vector3.Cross (moveDirection, rootDirection);
+		
+		Debug.DrawRay (new Vector3 (root.position.x, root.position.y + 1.5f, root.position.z), moveDirection, Color.green);
+		Debug.DrawRay (new Vector3 (root.position.x, root.position.y + 1.5f, root.position.z), rootDirection, Color.magenta);
+		Debug.DrawRay (new Vector3 (root.position.x, root.position.y + 1.5f, root.position.z), stickDirection, Color.blue);
+
+		float angleRootToMove = Vector3.Angle (rootDirection, moveDirection) * (axisSign.y >= 0 ? -1f : 1f);
+
+		angleRootToMove /= 180f;
+
+		directionOut = angleRootToMove * directionSpeed;
+	}
+	
+	public bool IsInLocomotion ()
+		{
+			return stateInfo.fullPathHash == m_LocomotionId;
+		}
+
+
+	
+	#endregion Methods
+	
+}
---
+++ Third Person John Mac/Assets/Scripts/ThirdPersonCameraController.cs
@@ -0,0 +1,184 @@
+﻿using UnityEngine;
+using System.Collections;
+
+struct CameraPosition
+{
+	private Vector3 position;
+	private Transform xForm;
+
+	public Vector3 Position  { get {return position; } set {position = value;} }
+	public Transform XForm { get { return xForm; } set { xForm = value; } }
+
+	public void Init(string camName, Vector3 pos, Transform transform, Transform parent)
+	{
+		position = pos;
+		xForm = transform;
+		xForm.name = camName;
+		xForm.parent = parent;
+		xForm.localPosition = Vector3.zero;
+		xForm.localPosition = position;
+		
+	}
+
+}
+
+
+[RequireComponent (typeof (BarsEffect))]
+public class ThirdPersonCameraController: MonoBehaviour {
+	
+	#region Variables (private)
+	[SerializeField]
+	private PlayerControllerLogic follow;
+	[SerializeField]
+	private float distanceAway;
+	[SerializeField]
+	private float distanceUp;
+	[SerializeField]
+	private Transform followTransform;
+	[SerializeField]
+	private float firstPersonThreshold;
+	[SerializeField]
+	private Vector3 cameraOffset = new Vector3(0f ,1.5f, 0f);
+	[SerializeField]
+	private float widescreenTime = 0.2f;
+	[SerializeField]
+	private float targetingTime = 0.5f;
+	
+
+	private Vector3 velocityCamSmooth = Vector3.zero;
+	[SerializeField]
+	private float camSmoothDampTime = 0.1f;
+	
+	private Vector3 lookDir;
+	private Vector3 targetPosition;
+	private BarsEffect barEffect;
+	private CamStates camState = CamStates.Behind;
+	private float xAxisRot = 0.0f;
+	private CameraPosition firstPersonCamPos;
+	private float lookWeight;
+
+	#endregion
+	
+	#region Properties (public)
+
+	public enum CamStates 
+	{
+		Behind,
+		FirstPerson,
+		Target,
+		Free
+	}
+	
+	#endregion
+	
+	#region Unity events
+	
+	
+	void Start ()
+	{
+		follow = GameObject.FindWithTag ("Player").GetComponent<PlayerControllerLogic> ();
+		followTransform = GameObject.FindWithTag ("Player").transform;
+		lookDir = followTransform.forward;
+
+		barEffect = GetComponent<BarsEffect> ();
+		if (barEffect == null)
+		{
+			Debug.LogError("Attach a widescreen BarsEffect script to the camera.", this);
+		}
+
+		firstPersonCamPos = new CameraPosition ();
+		firstPersonCamPos.Init
+			(
+				"First Person Camera",
+				new Vector3 (0.0f, 1.6f, 02f),
+				new GameObject ().transform,
+				followTransform
+
+		);
+	}
+
+
+	void Update()
+	{
+
+	}
+	
+void LateUpdate()
+	{
+		float rightX = Input.GetAxis ("RightStickX");
+		float rightY = Input.GetAxis ("RightStickY");
+		float leftX = Input.GetAxis ("Horizontal");
+		float leftY = Input.GetAxis ("Vertical");
+
+		Vector3 characterOffset = followTransform.position + cameraOffset;
+
+		if (Input.GetAxis ("Target") > 0.1f)
+		{
+			barEffect.coverage = Mathf.SmoothStep (barEffect.coverage, widescreenTime, targetingTime);
+
+			camState = CamStates.Target;
+		} 
+		else
+		{
+			barEffect.coverage = Mathf.SmoothStep (barEffect.coverage, 0f, targetingTime);
+
+			if (rightY > firstPersonThreshold && camState != CamStates.Free && !follow.IsInLocomotion())
+			{
+				xAxisRot = 0;
+				lookWeight = 0f;
+				camState = CamStates.FirstPerson;
+			}
+
+			camState = CamStates.Behind;
+		}
+
+
+		switch (camState) 
+		{
+			case CamStates.Behind:
+
+				lookDir = characterOffset - this.transform.position;
+				lookDir.y = 0;
+				lookDir.Normalize ();
+				Debug.DrawRay (this.transform.position, lookDir, Color.green);
+				//Debug.DrawRay (follow.position, Vector3.up * distanceUp, Color.red);
+				//Debug.DrawRay (follow.position, -1f * follow.forward * distanceAway, Color.blue);
+				//Debug.DrawLine (follow.position, targetPosition, Color.magenta);
+			break;
+
+		case CamStates.Target:
+			lookDir = followTransform.forward;
+			break;
+		}
+
+		targetPosition = characterOffset + followTransform.up * distanceUp - lookDir * distanceAway;
+
+		CompensateForWalls (characterOffset, ref targetPosition);
+
+		smoothPosition (this.transform.position, targetPosition);
+
+
+		transform.LookAt (followTransform);
+	}
+	
+	#endregion Unity events
+	
+	#region Methods
+
+	private void smoothPosition(Vector3 fromPos, Vector3 toPos)
+	{
+		this.transform.position = Vector3.SmoothDamp (fromPos, toPos, ref velocityCamSmooth, camSmoothDampTime);
+	}
+
+	private void CompensateForWalls(Vector3 fromObject, ref Vector3 toTarget)
+	{
+		Debug.DrawLine (fromObject, toTarget, Color.cyan);
+		RaycastHit wallHit = new RaycastHit ();
+		if (Physics.Linecast (fromObject, toTarget, out wallHit)) {
+			Debug.DrawRay (wallHit.point, Vector3.left, Color.red);
+			toTarget = new Vector3 (wallHit.point.x, toTarget.y, wallHit.point.z);
+		}
+	}
+	#endregion Methods
+	
+}
